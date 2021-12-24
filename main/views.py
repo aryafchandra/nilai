d21d5c673ad547786c2e49d1390e2eec1acbc9b2
aryafchandra <aryafakhruddin700@gmail.com>
2021-12-24 15:47:44 +0700

    init app1

--- main/views.py
+++ main/views.py
@@ -14,48 +14,49 @@ def home(request):
 
     if request.method == "POST":
         text = request.POST["detail"]
-        print(text)
+        
     
-    
-
-    scr = 0
-    x = text.split('\n')
-    detail = []
-    
-    for i in x:
-        a = i.split()
-        detail.append(a)
-    print(detail)
-    temp = []    
-    precont = ''
-    ctr = 0
-    tmp1 = []
-
-    for x in detail:
-        print(x)
+    if text == '':
+        pass
+    else:
+        scr = 0
+        x = text.split('\n')
+        detail = []
+        
         for i in x:
-            if ctr == 0:
-                precont += f'{i}'
-                ctr +=1
-            else:
-                precont += f' {i}'
-        ctr = 0
-        temp.append(precont)
+            a = i.split()
+            detail.append(a)
+        print(detail)
+        temp = []    
         precont = ''
-    
-    for i in detail:
-        weight = i[-2][:-1]
-        try:
-            float(weight)
-            scr += float(weight) / 100 * float(i[-1])
-        except:
-            scr += 0
+        ctr = 0
+        tmp1 = []
 
-    print(scr)
-    context = {
-        'finals':[scr],
-        'butir':temp
-    }
+        for x in detail:
+            print(x)
+            for i in x:
+                if ctr == 0:
+                    precont += f'{i}'
+                    ctr +=1
+                else:
+                    precont += f' {i}'
+            ctr = 0
+            temp.append(precont)
+            precont = ''
+        
+        for i in detail:
+            weight = i[-2][:-1]
+            try:
+                float(weight)
+                scr += float(weight) / 100 * float(i[-1])
+            except:
+                scr += 0
+
+        print(scr)
+        context = {
+            'finals':[scr],
+            'butir':temp
+        }
     
     
     return render(request, 'main/home.html', context)
